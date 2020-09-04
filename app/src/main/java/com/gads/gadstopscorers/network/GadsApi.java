2f739b47201e887bd3309063c8b5ffff16e0b83f
MichaelOdera <michogelira@gmail.com>
2020-09-04 12:34:57 +0300

    added the interface GadsApi.java to be alow for the implementation of the calls to obtain gads leader results in terms of hours and also skills

--- app/src/main/java/com/gads/gadstopscorers/network/GadsApi.java
+++ app/src/main/java/com/gads/gadstopscorers/network/GadsApi.java
@@ -1,4 +1,17 @@
 package com.gads.gadstopscorers.network;
 
+import com.gads.gadstopscorers.models.LeadersApiResponse;
+import com.gads.gadstopscorers.models.SkillsApiResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface GadsApi {
+    @GET("api/hours")
+    Call<List<LeadersApiResponse>> getLeadersResults();
+
+    @GET("api/skilliq")
+    Call<List<SkillsApiResponse>> getSkillsResults();
 }
