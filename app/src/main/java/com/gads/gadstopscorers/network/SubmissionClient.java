fd463cc3938bdf1d0e25cec2c364e69fc322d810
MichaelOdera <michogelira@gmail.com>
2020-09-04 12:39:44 +0300

    added the Retrofit instance SubmissionClient that contains the builder class for the baseURL and the GsonConverter factory and the creation of the SubmissionApi interface class

--- app/src/main/java/com/gads/gadstopscorers/network/SubmissionClient.java
+++ app/src/main/java/com/gads/gadstopscorers/network/SubmissionClient.java
@@ -1,4 +1,20 @@
 package com.gads.gadstopscorers.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class SubmissionClient {
+
+    private static Retrofit retrofit = null;
+
+    public static SubmissionApi getClient(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.SUBMISSION_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(SubmissionApi.class);
+
+    }
 }
