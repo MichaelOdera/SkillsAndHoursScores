ac1c82f1dbee58f07dc30bd33aad8238f77de712
MichaelOdera <michogelira@gmail.com>
2020-09-04 12:36:52 +0300

    added the Retrofit instance GadsClient that contains the builder class for the baseURL and the GsonConverter factory and the creation of the GadsApi interface class

--- app/src/main/java/com/gads/gadstopscorers/network/GadsClient.java
+++ app/src/main/java/com/gads/gadstopscorers/network/GadsClient.java
@@ -1,4 +1,19 @@
 package com.gads.gadstopscorers.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class GadsClient {
+    private static Retrofit retrofit = null;
+
+    public static GadsApi getClient(){
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.GADS_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(GadsApi.class);
+    }
 }
