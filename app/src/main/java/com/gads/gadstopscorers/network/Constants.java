1960209557392e75258764f085606e6180b0b5f5
MichaelOdera <michogelira@gmail.com>
2020-09-04 12:33:23 +0300

    added the file Constants.java that holds the constant variable that are used for the fetching of data. The constants are basically the baseURL for the heroku based api and the google docs api

--- app/src/main/java/com/gads/gadstopscorers/network/Constants.java
+++ app/src/main/java/com/gads/gadstopscorers/network/Constants.java
@@ -1,4 +1,6 @@
 package com.gads.gadstopscorers.network;
 
 public class Constants {
+    public static final String GADS_URL = "https://gadsapi.herokuapp.com/";
+    public static final String SUBMISSION_URL = "https://docs.google.com/forms/d/e/";
 }
