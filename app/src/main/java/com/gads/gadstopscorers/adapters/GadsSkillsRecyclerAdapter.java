1b69a4b50741a7701085db5230b1d61e73e88710
MichaelOdera <michogelira@gmail.com>
2020-09-04 12:28:12 +0300

    added the GadsSkillsRecyclerAdapter.java to be able to attach the layout to the data for the leaders skills' details being fetched from the api resource to display on the view

--- app/src/main/java/com/gads/gadstopscorers/adapters/GadsSkillsRecyclerAdapter.java
+++ app/src/main/java/com/gads/gadstopscorers/adapters/GadsSkillsRecyclerAdapter.java
@@ -1,4 +1,77 @@
 package com.gads.gadstopscorers.adapters;
 
-public class GadsSkillsRecyclerAdapter {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gads.gadstopscorers.R;
+import com.gads.gadstopscorers.models.SkillsApiResponse;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class GadsSkillsRecyclerAdapter extends RecyclerView.Adapter<GadsSkillsRecyclerAdapter.GadsSkillsViewHolder> {
+    private Context mContext;
+    private List<SkillsApiResponse> mSkillsLeaders;
+
+    public GadsSkillsRecyclerAdapter(Context context, List<SkillsApiResponse> skillsLeaders){
+        mContext = context;
+        mSkillsLeaders = skillsLeaders;
+    }
+
+    @NonNull
+    @Override
+    public GadsSkillsRecyclerAdapter.GadsSkillsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_skills_leader, parent, false);
+        GadsSkillsViewHolder gadsSkillsViewHolder = new GadsSkillsViewHolder(view);
+        return gadsSkillsViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GadsSkillsRecyclerAdapter.GadsSkillsViewHolder holder, int position) {
+        holder.bindSkillsLeader(mSkillsLeaders.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSkillsLeaders.size();
+    }
+
+    public class GadsSkillsViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.skillsImageView) ImageView mLeaderBySkillsImageView;
+        @BindView(R.id.skillsNameHourTextView) TextView mLeaderBySkillsNameTextView;
+        @BindView(R.id.skillsHoursScoreAndCountryTextView) TextView mLeaderBySkillsHoursAndCountryTextView;
+
+        public GadsSkillsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mContext = itemView.getContext();
+            ButterKnife.bind(this, itemView);
+        }
+
+        @SuppressLint("SetTextI18n")
+        public void bindSkillsLeader(SkillsApiResponse mSkillLeader) {
+            if (mSkillLeader.getBadgeUrl() != null){
+                Picasso.get().load(mSkillLeader.getBadgeUrl()).into(mLeaderBySkillsImageView);
+            }else{
+                Picasso.get().load(R.drawable.imageholder).into(mLeaderBySkillsImageView);
+            }
+
+            mLeaderBySkillsNameTextView.setTypeface(null, Typeface.BOLD);
+            mLeaderBySkillsNameTextView.setText(mSkillLeader.getName());
+
+            mLeaderBySkillsHoursAndCountryTextView.setText(mSkillLeader.getScore()+" skills IQ Score, "+ mSkillLeader.getCountry());
+
+        }
+    }
 }
